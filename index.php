ccf347b5cb6362d142b8384cc8025ef56549f6d8
magnetrwn <rpolli.dev@tutanota.com>
2023-05-02 17:15:44 +0200

    Added cross-source session prevention.

--- index.php
+++ index.php
@@ -1,144 +1,142 @@
+<?php
+      /////////////
+    //             //
+    //   Session   //
+    //             //
+      /////////////
+
+    include("model/mgmt-session.php");
+    if(!check_dssessionid() || isset($_GET["destroy"]))
+        // Controlliamo se l'host remoto ha cambiato indirizzo, se si: eliminiamo il cookie di sessione e rigeneriamo
+        delete_dssessionid();
+    create_dssessionid();
+    session_start();
+?>
 <!DOCTYPE html>
 <html>
 <head>
     <link rel="icon" type="image/png" href="/static/img/favicon.png">
 </head>
 <body class="flex flex-col min-h-screen">
-	
+<div>
 <?php
-/////////////////////
-// Header (navbar) //
-/////////////////////
-session_name("dssessionid");
-// Attiviamo httpOnly, per prevenire accesso via JS
-session_set_cookie_params(0, "/", "", false, true);
-// Generiamo un ID di sessione di 64 byte: i primi 32 sono hash dell'IP remoto, e gli ultimi 32 sono casuali
-// TODO: sistema protezione dal furto del session id (impedire che un IP remoto diverso si connetta all'utente già loggato usando stesso session id)
-if(!isset($_COOKIE["dssessionid"])) {
-    $sessfromip = md5(strtr($_SERVER['REMOTE_ADDR'], ".:/", "=")) 
-                  .bin2hex(openssl_random_pseudo_bytes(16));
-    session_id($sessfromip);
-}
-session_start();
-$loginlabel = "Login";
-if(isset($_SESSION["user"]))
-    $loginlabel = "Logout";
-include("view/page-top.php");
-?>
+      ////////////////////
+    //                    //
+    // Costruzione pagina //
+    //                    //
+      ////////////////////
 
-<div><?php
-///////////////////////////////////////
-// Sezione principale del controller //
-///////////////////////////////////////
-if(!isset($_GET["goto"]))
-    // Se non viene richiesta pagina, va alla homepage
-    $goto = "homepage";
-else
-    // Traduce richieste in minuscolo
-    $goto = strtolower($_GET["goto"]);
+    $loginlabel = "Login";
+    if(isset($_SESSION["user"]))
+        // Imposta logout invece di login
+        $loginlabel = "Logout";
+    include("view/page-top.php");
 
-switch($goto) {
+    if(!isset($_GET["goto"]))
+        // Se non viene richiesta pagina, va alla homepage
+        $goto = "homepage";
+    else
+        // Traduce richieste in minuscolo
+        $goto = strtolower($_GET["goto"]);
 
-    case "homepage":
-        // Pagina home
-        $welcome = "Benvenuto su Datasheet Search!";
-        if(isset($_SESSION["user"]))
-            // Mostra username se loggato
-            $welcome = "Bentornato, ".$_SESSION["user"].".";
-        include("view/page-homepage.php");
-        break;
+    switch($goto) {
 
-    case "login":
-        // Pagina login
-        include_once("model/util-js.php");
-        if(isset($_SESSION["user"])) {
-            // Se già loggato, effettua il logout
-            unset($_SESSION["user"]);
-            session_destroy();
-            redirect_js("index.php?goto=homepage");
-        }
-        else {
-            include("view/auth/page-login.php");
-        }
-        break;
+        case "homepage":
+            // Pagina home
+            $welcome = "Benvenuto su Datasheet Search!";
+            if(isset($_SESSION["user"]))
+                // Mostra username se loggato
+                $welcome = "Bentornato, ".$_SESSION["user"].".";
+            include("view/page-homepage.php");
+            break;
 
-    case "auth":
-        // Autenticazione successiva al login/register
-        include_once("model/util-js.php");
-        include_once("model/mgmt-auth.php");
-        if(isset($_GET["newuser"])) {
-            // Passa al processore register
-            if(mysql_user_register($_POST["username"], $_POST["email"], $_POST["password"]))  {
-                // Register successo
-                redirect_js("index.php?goto=success&msg=register&btngoto=login");
+        case "login":
+            // Pagina login
+            include_once("model/util-js.php");
+            if(isset($_SESSION["user"])) {
+                // Se già loggato, effettua il logout e rigenera il session_id usando destroy, vedi Session in alto
+                unset($_SESSION);
+                session_destroy();
+                redirect_js("index.php?goto=homepage&destroy");
             }
-            else
-                // Register fallito (difficile), torna alla pagina register
-                redirect_js("index.php?goto=register&badregister");
-        }
-        else {
-            // Passa al processore login
-            if(mysql_user_login($_POST["username"], $_POST["password"]))  {
-                // Login successo
+            else {
+                include("view/auth/page-login.php");
+            }
+            break;
+
+        case "auth":
+            // Autenticazione successiva al login/register
+            include_once("model/util-js.php");
+            include_once("model/mgmt-auth.php");
+            if(isset($_GET["newuser"])) {
+                // Passa al processore register
+                if(mysql_user_register($_POST["username"], $_POST["email"], $_POST["password"]))
+                    // Register successo
+                    redirect_js("index.php?goto=success&msg=register&btngoto=login");
+                else
+                    // Register fallito (probabile attacker/bot), torna alla pagina register
+                    redirect_js("index.php?goto=register&badregister");
+            }
+            else {
+                // Passa al processore login
+                if(mysql_user_login($_POST["username"], $_POST["password"]))
+                    // Login successo
+                    redirect_js("index.php?goto=homepage");
+                else
+                    // Login fallito, torna alla pagina login
+                    redirect_js("index.php?goto=login&badlogin");
+            }
+            break;
+
+        case "register":
+            // Pagina di registrazione
+            include_once("model/util-js.php");
+            if(isset($_SESSION["user"])) {
+                // Se già loggato, torna alla homepage
                 redirect_js("index.php?goto=homepage");
             }
-            else
-                // Login fallito, torna alla pagina login
-                redirect_js("index.php?goto=login&badlogin");
-        }
-        break;
+            else {
+                include("view/auth/page-register.php");
+            }
+            break;
 
-    case "register":
-        // Pagina di registrazione
-        include_once("model/util-js.php");
-        if(isset($_SESSION["user"])) {
-            // Se già loggato, torna alla homepage
-            redirect_js("index.php?goto=homepage");
-        }
-        else {
-            include("view/auth/page-register.php");
-        }
-        break;
+        case "azienda":
+        case "listino":
+        case "componente":  
+        case "datasheet":
+        case "package":
+            // La view autolist costruisce tabelle di visualizzazione usando il $goto, quindi solo queste tabelle
+            include_once("model/util-search.php");
+            include_once("model/util-db.php");
+            include("view/search/page-autolist.php");
+            break;
 
-    case "azienda":
-    case "listino":
-    case "componente":  
-    case "datasheet":
-    //case "package":
-        include_once("model/util-search.php");
-        include_once("model/util-db.php");
-        include("view/search/page-autolist.php");
-        break;
+        case "search":
+            include("view/search/page-search.php");
+            break;
 
-    case "search":
-        include("view/search/page-search.php");
-        break;
+        case "success":
+            // Mostra una schermata di operazione riuscita in base a paramteri GET
+            include("view/page-success.php");
+            break;
 
-    case "success":
-        // Mostra una schermata di operazione riuscita in base a paramteri GET
-        include("view/page-success.php");
-        break;
-
-    default:
-        // Pagina non implementata
-        http_response_code(404); // non funziona?
-        include("view/page-404.html");
-        break;
-}
-
-?></div>
-
-<?php
-////////////
-// Footer //
-////////////
-include("view/page-bottom.html");
-
-/////////////
-// Scripts //
-/////////////
-include_once("view/using/tailwindcss-script.html");
+        default:
+            // Pagina non implementata
+            http_response_code(404); // non funziona?
+            include("view/page-404.html");
+            break;
+    }
 ?>
-	
+</div>
+<?php
+      ////////////
+    //            //
+    //   Script   //
+    //            //
+      ////////////
+
+    include("view/page-bottom.html"); // anche il footer qui
+    include_once("view/using/tailwindcss-script.html");
+?>
 </body>
 </html>
---
+++ model/mgmt-session.php
@@ -0,0 +1,29 @@
+<?php
+    define("DSSNAME", "dssessionid");
+    define("DSSEXPIRE", 518401);
+
+    // Usare session_start() e session_destroy() assieme a queste funzioni
+
+    function create_dssessionid() {
+        session_name(DSSNAME);
+        // Attiviamo httpOnly, per prevenire accesso via JS
+        session_set_cookie_params(DSSEXPIRE, "/", "", false, true);
+        // Generiamo un ID di sessione di 64 byte: i primi 32 sono hash dell'IP remoto, e gli ultimi 32 sono casuali
+        if(!isset($_COOKIE[DSSNAME])) {
+            $sessfromip = md5(strtr($_SERVER['REMOTE_ADDR'], ".:/", "=")) 
+                        .bin2hex(openssl_random_pseudo_bytes(16));
+            session_id($sessfromip);
+        }  
+    }
+
+    function check_dssessionid() {
+        // vs. Session Hijacking (è ancora possibile, non ci sono ancora controlli CSRF)
+        //return (md5(strtr($_SERVER['REMOTE_ADDR'], ".:/", "=")) === substr(session_id(), 0, 32)); // non funziona con session_id?
+        return (md5(strtr($_SERVER['REMOTE_ADDR'], ".:/", "=")) === substr($_COOKIE[DSSNAME], 0, 32));
+    }
+
+    function delete_dssessionid() {
+        unset($_COOKIE[DSSNAME]);
+        setcookie(DSSNAME, null, 1, "/");
+    }
+?>
